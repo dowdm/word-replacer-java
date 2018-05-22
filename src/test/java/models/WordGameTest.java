5deb6a6da7f3044e3c834ce450a9f015ddbb67cf
johnny ray alt and Matt Dowdney <matt@>
2018-05-22 16:36:32 -0700

    add third test for partials

--- src/test/java/models/WordGameTest.java
+++ src/test/java/models/WordGameTest.java
@@ -4,16 +4,25 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class WordGameTest {
+
     @Test
     public void replaceWord_returnsOriginalString_originalstring() throws Exception {
         WordGame testWordGame = new WordGame();
         String expected = "drink more coffee";
         assertEquals(expected, testWordGame.replaceWord("drink more coffee", "", ""));
     }
+
     @Test
     public void replaceWord_returnsStringWithNewWords_editedstring() throws Exception {
         WordGame testWordGame = new WordGame();
         String expected = "drink less coffee";
         assertEquals(expected, testWordGame.replaceWord("drink more coffee", "more","less"));
+    }
+
+    @Test
+    public void replaceWord_returnsStringWithReplacedWordsAndPartials_editedstring() throws Exception {
+        WordGame testWordGame = new WordGame();
+        String expected = "drink dog coffee dogegory";
+        assertEquals(expected, testWordGame.replaceWord("drink cat coffee category", "cat","dog"));
     }
 }
